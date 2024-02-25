468003363e9e28562ac9fa006de649056f7bdd56
K23dev <hashersmasher@proton.me>
2024-02-25 14:33:10 -0300

    fix version message

--- cmd/appbanner.go
+++ cmd/appbanner.go
@@ -6,7 +6,7 @@ func appBanner(version string) {
 
 	fmt.Println(" ### ### ### ####")
 	fmt.Println("    TANGO_CLI    ")
-	fmt.Printf("        V. %s    ", version)
+	fmt.Printf("        V. %s    \n", version)
 	fmt.Println(" ### ### ### ####")
 	fmt.Println("")
 }
